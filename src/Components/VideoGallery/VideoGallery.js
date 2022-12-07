f16610bc149735b3ef70d44ebd6ec8002f4475c2
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-12-06 23:32:19 -0800

    routes, landing page and login page

--- src/Components/VideoGallery/VideoGallery.js
+++ src/Components/VideoGallery/VideoGallery.js
@@ -1,28 +1,24 @@
-import HeroVid from "./VideoGallery.scss";
+import "./VideoGallery.scss";
 import React from "react";
 
 const VideoGallery = () => {
-    return (
-        <section class="gallery">
-            
-        <div>
-          <h1 class="gallery__title"> Video Gallery</h1>
+  return (
+    <section className="gallery">
+      <div>
+        <h3 className="gallery__title"> VIDEO GALLERY</h3>
+      </div>
+
+      <div className="gallery__wrapper">
+        <div className="gallery__video-wrapper">
+          <video className="gallery__video"></video>
         </div>
 
-        <div class="gallery__collection">
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
-           <video className="gallery__video"></video>
+        <div className="gallery__text-wrapper">
+          <p className="gallery__text"></p>
         </div>
-      </section>
-
-    )
+      </div>
+    </section>
+  );
 };
 
 export default VideoGallery;
--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,28 +1,36 @@
 import "./LandingPage.scss";
 import React from "react";
-// import Image from "../../Assets/Images/timothy-eberly-7MRajrPiTqw-unsplash.jpg";
+import { useEffect, useState } from "react";
+import LandingHeader from "../../Components/LandingHeader/LandingHeader";
+import LandingLogin from "../../Components/LandingLogin/LandingLogin";
 
 const LandingPage = () => {
-    
-    return (
-      <div className="landing">
-        <div className="landing__img">
-          <div className="landing__text">
-            {/* <img
-          className="hero__img"
-          src={Image}
-          alt="Black and white photo of 2 men in a Jiu-Jitsu fight"
-        ></img> */}
-            <h1 className="landing__caption">
-              Keep track of your training to level up your game.
-            </h1>
-            <button className="landing__button">
-              Sign In
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-}
+  const [isLandingShowValid, setIsLandingShowValid] = useState(true);
+  const [isLoginHideValid, setIsLoginHideValid] = useState(false);
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+    setIsLandingShowValid(false);
+    setIsLoginHideValid(true);
+  };
+
+  return (
+    <main className="landing">
+      <section
+        className={`landing__header
+            ${isLandingShowValid ? "" : "landing__header--hide"} `}
+      >
+        <LandingHeader handleSignIn={handleSignIn} />
+      </section>
+
+      <section
+        className={`landing__login 
+            ${!isLoginHideValid ? "" : "landing__login--show"} `}
+      >
+        <LandingLogin />
+      </section>
+    </main>
+  );
+};
 
 export default LandingPage;
