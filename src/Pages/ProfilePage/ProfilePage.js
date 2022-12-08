962655f062aed122810977235e39c749e72c811f
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-12-08 10:09:06 -0800

    working on Add new Video

--- src/Pages/ProfilePage/ProfilePage.js
+++ src/Pages/ProfilePage/ProfilePage.js
@@ -7,90 +7,142 @@ import HeroVid from "../../Components/HeroVid/HeroVid";
 import VideoGallery from "../../Components/VideoGallery/VideoGallery";
 // import { useParams } from "react-router-dom";
 
-
 const BACK_END = process.env.REACT_APP_BACKEND_URL;
 
 const ProfilePage = () => {
   const [isUserShowValid, setIsUserShowValid] = useState(true);
   const [isUploadHideValid, setIsUploadHideValid] = useState(false);
 
-   const handleUpload = (event) => {
-     event.preventDefault();
-     setIsUserShowValid(false);
-     setIsUploadHideValid(true);
-   };
-
+  const handleUpload = (event) => {
+    event.preventDefault();
+    setIsUserShowValid(false);
+    setIsUploadHideValid(true);
+  };
 
   const [videos, setVideos] = useState([]);
+  // const [newVideo, setnewVideo] = useState(null);
   const [editVideoId, setEditVideoId] = useState(null);
   // const [users, setUsers] = useState([]);
 
-    useEffect(() => {
-      const fetchVideos = async () => {
-        const { data } = await axios.get(`${BACK_END}/videos`);
-        setVideos(data);
-      };
-      // console.log(data);
-      
-      // const fetchUsers = async () => {
-        //   const { data } = await axios.get(`${BACK_END}/users`);
-        //   setUsers(data);
-        // };
-        
-        fetchVideos();
-        // fetchUsers();
-      }, []);
-      
-      const handleSubmit = (event) => {
-        event.preventDefault();
-        const newVideo = {
-          technique_name: event.target.techniqueName.value,
-          description: event.target.description.value,
-          video: event.target.video.value,
-          user_id: event.target.userId.value,
-        };
-        
-        
-        axios.post(`${BACK_END}/videos`, newVideo).then((response) => {
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const { data } = await axios.get(`${BACK_END}/videos`);
+      setVideos(data);
+    };
+    // console.log(data);
+
+    // const fetchUsers = async () => {
+    //   const { data } = await axios.get(`${BACK_END}/users`);
+    //   setUsers(data);
+    // };
+
+    fetchVideos();
+    // fetchUsers();
+  }, []);
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault();
+  //   const newVideo = {
+  //     technique_name: event.target.techniqueName.value,
+  //     description: event.target.description.value,
+  //     video: event.target.video.value,
+  //     user_id: event.target.userId.value,
+  //   };
+
+  //   axios.post(`${BACK_END}/videos`, newVideo).then((response) => {
+  //     setVideos([...videos, response.data]);
+  //   });
+  //   event.target.reset();
+  // };
+
+  // ----------------------------------------------------------------------------
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const newVideo = {
+      technique_name: event.target.techniqueName.value,
+      description: event.target.description.value,
+      video: event.target.video.value,
+      user_id: "2c0d90bd-558e-4991-b31a-7d55e162a45d",
+    };
+
+    const formData = new FormData();
+     formData.append("technique_name", newVideo.technique_name);
+     formData.append("description", newVideo.description);
+     formData.append("video", newVideo.video);
+     formData.append("user_id", newVideo.user_id);
+
+    try {
+      const response = await axios.post(
+        `${BACK_END}/videos`,
+        formData,
+        {
+          headers: {
+            "Content-Type": `multipart/form-data`,
+          },
+        }).then((response) => {
           setVideos([...videos, response.data]);
-        });
-        event.target.reset();
-      };
-      
-     
+        }
+      );
 
-      const handleEdit = (event, videoId) => {
-        event.preventDefault();
-        setEditVideoId(videoId);
-      };
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+// --------------------------------------------------------------------------------
 
-        const handleUpdate = (event, videoId) => {
-          event.preventDefault();
-          const values = {
-            technique_name: event.target.techniqueName.value,
-            description: event.target.description.value,
-            video: event.target.video.value,
-          };
-          axios
-            .patch(`${BACK_END}/videos/${videoId}`, values)
-            .then((response) => {
-              const updatedVideos = videos.map((video) =>
-                video.id === response.data.id ? response.data : video
-              );
-              setVideos(updatedVideos);
-              setEditVideoId(null);
-            });
-        };
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
 
-         const handleDelete = async (event, videoId) => {
-           event.preventDefault();
-           const {
-             data: { deletedVideoId },
-           } = await axios.delete(`${BACK_END}/videos/${videoId}`);
-           setVideos(
-             videos.filter((video) => video.id !== deletedVideoId)
-           );
-         };
+  // const formData = new FormData();
+  //  formData.append("technique_name", newVideo.technique_name);
+  //  formData.append("description", newVideo.description);
+  //  formData.append("video", newVideo);
+  //  formData.append("user_id", newVideo.userId);
+
+  //  try {
+  //   await axios.post(`${BACK_END}/videos`, formData, {
+  //     headers: {
+  //       "Content-Type": `multipart/form-data`,
+  //     },
+  //   });
+    
+  //  } catch (error) {
+  //   console.log(error);
+  //   window.alert(error);
+  //  }
+  // }
+
+
+  const handleEdit = (event, videoId) => {
+    event.preventDefault();
+    setEditVideoId(videoId);
+  };
+
+  const handleUpdate = (event, videoId) => {
+    event.preventDefault();
+    const values = {
+      technique_name: event.target.techniqueName.value,
+      description: event.target.description.value,
+      video: event.target.video.value,
+    };
+    axios.patch(`${BACK_END}/videos/${videoId}`, values).then((response) => {
+      const updatedVideos = videos.map((video) =>
+        video.id === response.data.id ? response.data : video
+      );
+      setVideos(updatedVideos);
+      setEditVideoId(null);
+    });
+  };
+
+  const handleDelete = async (event, videoId) => {
+    event.preventDefault();
+    const {
+      data: { deletedVideoId },
+    } = await axios.delete(`${BACK_END}/videos/${videoId}`);
+    setVideos(videos.filter((video) => video.id !== deletedVideoId));
+  };
 
   return (
     <div className="container">
