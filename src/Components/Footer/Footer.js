57d166bbd9094c83ef4e0004c5f7f900822ad0d2
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2022-12-09 15:56:45 -0800

    uploaded logos and images to assets folder, added logo to footer, fixed styling issues in footer component

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -8,9 +8,12 @@ import { FaTwitterSquare } from 'react-icons/fa';
 const Footer = () => {
     return (
       <footer className="footer">
+        <div className="footer__left">
+        <div className="footer__wrestlers"></div>
         <p className="footer__label">
           &copy; The BJJ LogApp Inc. All Rights Reserved.
         </p>
+        </div>
         <div className="footer__icons">
           <FaInstagramSquare />
           <FaFacebookSquare />
