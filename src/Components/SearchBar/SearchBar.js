581a51ba01c5a7d4319796b9f6f15e8debc586e3
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-18 17:46:38 -0700

    Delete unnecessary function and add toggling functionality

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,39 +1,51 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import "./SearchBar.scss";
 import { RxMagnifyingGlass } from "react-icons/rx";
 
 
 
 const SearchBar = ({ videos, setSearchResults }) => {
+  const inputRef = useRef(null);
+  const [isInputVisible, setInputVisible] = useState(false);
 
-  //  function to ignore the default action of the form subbmit event wich would be to reload the page
-  const handleSearch = (event) => event.preventDefault();
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const query = inputRef.current.value.toLowerCase();
 
- const handleSearchChange = (event) => {
-   const query = event.target.value.toLowerCase();
-   if (!query) {
-     return setSearchResults(videos);
-   }
+    if (!query) {
+      setSearchResults(videos);
+    } else {
+      const searchResultsArr = videos.filter((video) =>
+        video.technique_name.toLowerCase().includes(query)
+      );
+      setSearchResults(searchResultsArr);
+    }
 
-  //  new array to keep the videos that includes the query
-   const searchResultsArr = videos.filter(
-     (video) => video.technique_name.toLowerCase().includes(query)
-   );
-   setSearchResults(searchResultsArr);
- };
+    inputRef.current.value = "";
+    setInputVisible(false);
+  };
+
+  const toggleInputVisibility = () => {
+    setInputVisible(!isInputVisible);
+  };
 
   return (
-    <form className="search" onSubmit={handleSearch}>
-      <input
-        className="search__input"
-        placeholder="Search technique name"
-        type="text"
-        id="search"
-        onChange={handleSearchChange}
-      />
-      <button className="search__button">
+    <div className="search">
+      {isInputVisible && (
+        <form className="search__form" onSubmit={handleSearch}>
+          <input
+            ref={inputRef}
+            className="search__input"
+            placeholder="Search technique name"
+            type="text"
+            id="search"
+          />
+        </form>
+      )}
+      <button className="search__button" onClick={toggleInputVisibility}>
         <RxMagnifyingGlass />
       </button>
-    </form>
+    </div>
   );
 };
 
