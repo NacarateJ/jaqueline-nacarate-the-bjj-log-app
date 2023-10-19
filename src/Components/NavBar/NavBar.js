71b1d3db8e63be7c3ff55085b560bbb81f0fb6f1
Jaqueline Nacarate <jaquelinenacarate@gmail.com>
2023-10-18 17:44:02 -0700

    Import saerch bar and connect to Redux store

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,12 +1,19 @@
 import "./NavBar.scss";
 import React from "react";
+import { connect } from "react-redux";
 import { GrMenu } from "react-icons/gr";
 import Logo from "../../Assets/Logo/TheBjjLogAppLogo4.jpg";
+import SearchBar from "../SearchBar/SearchBar"
 
-const NavBar = () => {
+const NavBar = ({ videos, setSearchResults }) => {
   return (
     <div className="navbar">
-      <img className="navbar__logo" alt="App logo: jiu-jitsu fighters"src={Logo}/>
+      <img
+        className="navbar__logo"
+        alt="App logo: jiu-jitsu fighters"
+        src={Logo}
+      />
+      <SearchBar videos={videos} setSearchResults={setSearchResults} />
       <div className="navbar__bars">
         <GrMenu />
       </div>
@@ -14,4 +21,18 @@ const NavBar = () => {
   );
 };
 
-export default NavBar;
+const mapStateToProps = (state) => {
+  return {
+    videos: state.videos,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setSearchResults: (results) => {
+      dispatch({ type: "SET_SEARCH_RESULTS", payload: results });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
